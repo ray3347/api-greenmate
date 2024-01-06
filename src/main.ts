eaf349e650a4b48d507f9d443340ab796ca40b41
ray3347 <ansel.knb@gmail.com>
2024-01-06 17:20:42 +0700

    fix: heroku config

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT || 7000);
 }
 bootstrap();
